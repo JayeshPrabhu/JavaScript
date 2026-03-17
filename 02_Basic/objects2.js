13634426fb5e31267928f32512c0b69e09b10c75
JayeshPrabhu <115716043+JayeshPrabhu@users.noreply.github.com>
2026-03-17 15:07:27 +0000

    object

--- 02_Basic/objects2.js
+++ 02_Basic/objects2.js
@@ -1,59 +1,92 @@
-const tinderUser = new Object();
+// Creating an empty object (two ways: new Object() or {})
+const tinderUser = {}   
 
- tinderUser.id ="123abc"
- tinderUser.name="Jammy"
- tinderUser.isLoggedIn=false
+// Adding values (properties) into the object
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
 
-const regularUser ={
-    email:"some@gmail.com",
-    fullname:{
-        userName:{
-            firstname:"jayesh",
-            lastname:"prabhu"
+// Nested object example
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
         }
     }
 }
 
-//console.log(regularUser.fullname.userName);
+// Accessing nested values
+// regularUser.fullname.userfullname.firstname  → "hitesh"
 
 
-const obj1={1:"a",2:"b"}
-const obj2={33:"c",4:"d"}
+// Creating simple objects
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
 
-//const obj3 = {obj1,obj2}
+// Merging objects using spread operator (...)
+// This combines obj1 + obj2 into a new object
+const obj3 = {...obj1, ...obj2}
+// Now obj3 = {1:"a", 2:"b", 3:"a", 4:"b"}
 
-// const obj3 = Object.assign({},obj1,obj2)
-// console.log(obj3);
 
-// const obj3 ={...obj1,...obj2}
-// console.log(obj3);
-
-const user =[
+// Array of objects (common in databases)
+const users = [
     {
-        id:1,
-        email:"jauyesh"
+        id: 1,
+        email: "h@gmail.com"
     },
-      {
-        id:1,
-        email:"jauyesh"
+    {
+        id: 1,
+        email: "h@gmail.com"
     },
-      {
-        id:1,
-        email:"jauyesh"
+    {
+        id: 1,
+        email: "h@gmail.com"
     },
-      {
-        id:1,
-        email:"jauyesh"
-    }
 ]
 
-user[1].email
-console.log(tinderUser);
+// Accessing 2nd user's email
+users[1].email
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.has);
+// Object utility methods:
+
+// Object.keys(tinderUser) → returns only keys as array
+// Object.values(tinderUser) → returns only values
+// Object.entries(tinderUser) → returns key-value pairs in array form
+
+// tinderUser.hasOwnProperty('isLoggedIn') → checks if key exists
+
+
+// Object destructuring (easy way to extract values)
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// Extract courseInstructor and rename it as "instructor"
+const {courseInstructor: instructor} = course
+
+console.log(instructor)  
+// Output: hitesh
+
+
+// Example of how JSON data looks:
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+
+// Array of empty objects
+[
+    {},
+    {},
+    {}
+]
